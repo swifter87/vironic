ac89f6514596875600c35d9f935a1f23d741a83d
dima <osdima1@gmail.com>
2019-10-12 12:48:05 +0300

    first commit

---
+++ src/build/precache-manifest.166bd55e74ac5d75308f6e5f1d6a769d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cda876d5b0a9cef79e03f7cf1913c2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "806df41478d6b43b54c5",
+    "url": "/static/css/main.72307bf5.chunk.css"
+  },
+  {
+    "revision": "a9794584fa21621cac1e",
+    "url": "/static/js/2.56438e88.chunk.js"
+  },
+  {
+    "revision": "806df41478d6b43b54c5",
+    "url": "/static/js/main.8b140a8f.chunk.js"
+  },
+  {
+    "revision": "fead68ee3401313bf2da",
+    "url": "/static/js/runtime~main.a9b024df.js"
+  },
+  {
+    "revision": "73948da182714dad9ddaadb619b5ee48",
+    "url": "/static/media/mobile.73948da1.png"
+  },
+  {
+    "revision": "ed72796e91eebe6d75b9463c81e333ea",
+    "url": "/static/media/swifter_black.ed72796e.png"
+  },
+  {
+    "revision": "b493b3f31e04758bca0e43956121c707",
+    "url": "/static/media/video.b493b3f3.png"
+  },
+  {
+    "revision": "04847a91db8e49780d635bc0bed1639c",
+    "url": "/static/media/video2.04847a91.gif"
+  },
+  {
+    "revision": "9f1e4360f25a53330251e8201d09d7ae",
+    "url": "/static/media/video2.9f1e4360.mp4"
+  }
+]);
--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.use(express.static("./"));
-
-app.get("/", (req, res) => res.sendFile(__dirname + "dwew"));
 
 var nodemailer = require("nodemailer");
 
