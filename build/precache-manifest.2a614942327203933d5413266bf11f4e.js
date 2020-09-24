719c00362db9eb8b441fcde24e8530dcba30360e
dima <osdima1@gmail.com>
2020-09-24 16:07:21 +0300

    added build to git

---
+++ build/precache-manifest.2a614942327203933d5413266bf11f4e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f022ea084fc1dca86adbdac24b86248c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76b838d4d9ce42fd8748",
+    "url": "/static/css/main.89e9bbfe.chunk.css"
+  },
+  {
+    "revision": "5fa6a3c3242f991361c2",
+    "url": "/static/js/2.2518eb84.chunk.js"
+  },
+  {
+    "revision": "76b838d4d9ce42fd8748",
+    "url": "/static/js/main.13d61923.chunk.js"
+  },
+  {
+    "revision": "fead68ee3401313bf2da",
+    "url": "/static/js/runtime~main.a9b024df.js"
+  },
+  {
+    "revision": "9d304beb67126839f940e517db1fbdb5",
+    "url": "/static/media/logo.9d304beb.png"
+  },
+  {
+    "revision": "73948da182714dad9ddaadb619b5ee48",
+    "url": "/static/media/mobile.73948da1.png"
+  },
+  {
+    "revision": "ba12ff7a0a60e158866fe347a99b53df",
+    "url": "/static/media/user3.ba12ff7a.gif"
+  },
+  {
+    "revision": "04847a91db8e49780d635bc0bed1639c",
+    "url": "/static/media/video2.04847a91.gif"
+  }
+]);
